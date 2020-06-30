289b9c0ac21be2aaae8d63f245bc5e03020a0524
Edson Junior <edson_junior91@hotmail.com>
2020-06-30 00:33:15 -0300

    Multiplicação de frações

---
+++ Exercicio4_2/src/Aplicacao.java
@@ -0,0 +1,19 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		Fracao f1 = new Fracao();
+		f1.definirValores(5,2);
+		
+		Fracao f2 = new Fracao();
+		f2.definirValores(1,7);
+		
+		Fracao f3 = f1.multiplicar(f2);
+		
+		System.out.println(f3.obterFracao());
+		
+		System.out.println(f3.calcularValor());
+
+	}
+
+}
---
+++ Exercicio4_2/src/Fracao.java
@@ -0,0 +1,30 @@
+
+public class Fracao {
+	double numerador;
+	double denominador;
+	
+	
+	double calcularValor() {
+		if(denominador == 0) {
+			return 0;
+		}
+		return numerador / denominador;
+	}
+	
+	Fracao multiplicar(Fracao f) {
+		Fracao result = new Fracao();
+		
+		result.numerador = this.numerador * f.numerador;
+		result.denominador = this.denominador * f.denominador;
+		
+		return result;
+	}
+	void definirValores(double numerador, double denominador) {
+		this.numerador = numerador;
+		this.denominador = denominador;
+	}
+	
+	String obterFracao() {
+		return numerador + "/" + denominador;
+	}
+}
